c1419aaf3aa83b115e04d7fc18eba410f1d81bc4
TisseurDeToile <webmaster@tisseurdetoile.net>
2021-03-20 11:24:10 +0100

    feat(Choice): Ajout d'un système de choix du jeu

---
+++ src/components/Choice.vue
@@ -0,0 +1,24 @@
+<template>
+  <button v-on:click="$emit('msg-game-choice', this.game)">{{ game }}</button>
+</template>
+
+<script>
+export default {
+  props: { game: { type: String, required: true } },
+  emits: ['msg-game-choice'],
+}
+</script>
+
+<style scoped>
+button {
+  background-color: tomato;
+  width: 104px;
+  height: 104px;
+  border-radius: 10%;
+  line-height: 104px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
